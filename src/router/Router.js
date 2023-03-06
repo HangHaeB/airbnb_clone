abfa0ff49fc6a59890bb071ffe8f5979d48c6615
chanyeol123 <chanyeol123@gmail.com>
2023-03-06 09:42:55 +0900

    detail, mypage 라우트 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../page/Main";
+import Detail from "../page/Detail";
+import MyPage from "../page/MyPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="detail" element={<Detail/>} />
+        <Route path="mypage" element={<MyPage/>} />
+      </Routes>
     </BrowserRouter>
   );
 };
+
+export default Router;
