2189d9b1dacccad7654665fcef9a99e47d66dcca
chanyeol123 <chanyeol123@gmail.com>
2023-03-06 09:36:11 +0900

    로그인, 회원가입 관련 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,40 @@
 import instance from "./instance";
+import axios from "axios";
+import SignUp from "../components/modal/SignUp";
 
-const login = async (userId) => {
-  await instance.post(`/api/user/login`, userId);
+//로그인 요청
+const postLogin = async () => {
+ const data = {
+    email: "iamuser@naver.com",
+    password: "1234",
+  }
+  await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/users/login`, data)
+    .then((res) => {
+      alert("로그인 완료.")
+      
+    })
+    .catch((err)=>{
+      alert("로그인 실패")
+    })
 };
 
-export { login };
+//회원가입
+const postSignUp = async () => {
+  const data = {
+    email: "email@naver.com",
+    username: "username",
+
+    password: "12341234",
+    birth: "00000000",
+  }
+  await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/users/signup`, data)
+    .then((res) => {
+      alert(res.response.data.msg)
+      
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+};
+
+export { postLogin, postSignUp };
